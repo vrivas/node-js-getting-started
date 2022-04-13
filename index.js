af9ad5ee348b356c4cd3ff8ed554a3a40fe263cb
Victor M. Rivas <vrivas@ujaen.es>
2022-04-13 17:32:12 +0200

    Intentando conectar con airtable

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5500
+const base = require('airtable').base(process.env.AIRTABLE_BASE);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -8,6 +9,17 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/times', (req, res) => res.send(showTimes()))
+  .get('/test-airtable/:tableName', function (request, response) {
+    const { tableName } = request.params;
+    base(tableName).select().firstPage().then(([firstEntry]) => {
+      response.json({
+        message: `${firstEntry._table.name} ${JSON.stringify(firstEntry.fields)}`
+      });
+    }).catch(err => {
+      err.mensaje='Victor'
+      response.status(500).json(err)
+    });
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 showTimes = () => {
