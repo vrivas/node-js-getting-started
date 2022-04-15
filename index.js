90e82fda5cd7be3c0c8bed6bd393f6adbbbf1f80
Victor M. Rivas <vrivas@ujaen.es>
2022-04-16 00:39:38 +0200

    Modificaciones a index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5500
-const base = require('airtable').base(process.env.AIRTABLE_BASE);
+const base = require('airtable').base(process.env.AIRTABLE_BASE_ID);
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -9,17 +9,7 @@ express()
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
   .get('/times', (req, res) => res.send(showTimes()))
-  .get('/test-airtable/:tableName', function (request, response) {
-    const { tableName } = request.params;
-    base(tableName).select().firstPage().then(([firstEntry]) => {
-      response.json({
-        message: `${firstEntry._table.name} ${JSON.stringify(firstEntry.fields)}`
-      });
-    }).catch(err => {
-      err.mensaje='Victor'
-      response.status(500).json(err)
-    });
-  })
+  .get('/test-airtable/:tableName', (req,res)=>testAirtable(req,res))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 showTimes = () => {
@@ -29,4 +19,41 @@ showTimes = () => {
     result += i + ' ';
   }
   return result;
+}
+
+
+testAirtable = (req,res) => {
+ /* var Airtable = require('airtable');
+  var base = new Airtable({apiKey: 'keyAkPfL8VQQkA9jG'}).base('appnRqx6wupbDHFzY');
+
+  base(tableName).select({
+      // Selecting the first 3 records in Grid view:
+      maxRecords: 3,
+      view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+      // This function (`page`) will get called for each page of records.
+  
+      records.forEach(function(record) {
+          console.log('Retrieved', record.get('id'));
+      });
+  
+      // To fetch the next page of records, call `fetchNextPage`.
+      // If there are more records, `page` will get called again.
+      // If there are no more records, `done` will get called.
+      fetchNextPage();
+  
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+  });
+*/
+  const { tableName } = req.params;
+
+  base(tableName).select().firstPage().then(([firstEntry]) => {
+    return res.json({
+      message: `${firstEntry._table.name} ${JSON.stringify(firstEntry.fields)}`
+    });
+  }).catch(err => {
+    err.mensaje=tableName
+    return res.status(500).json(err)
+  });
 }
